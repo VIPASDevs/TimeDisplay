bc8e3c7ff453b3a5d2bd2455dd3cb6f49cdd0598
VIPAS <voiddevr@protonmail.com>
2026-08-07 21:10:20 -0400

    combo
    combo

---
+++ combo.c
@@ -0,0 +1,35 @@
+/*If you use VSCode for testing/tweaking this, please do not use the 
+integrated terminal, it can screw with how it looks for you when testing.*/
+
+#include <stdio.h>
+#include <time.h> //gives us time(), localtime(), time_t, and struct tm
+#include <stdlib.h> //general-purpose stuff
+#include "sleep.c" //my global sleep function (fuck standard sleep functions in C)
+
+int main(void) {
+    while (1) {
+        time_t now = time(NULL); //get the current time
+
+        struct tm *local = localtime(&now); //convert time to local time
+        struct tm *today = localtime(&now);
+
+        printf("\033[2J\033[H"); //clears the screen
+
+
+        printf("DATE: %04d-%02d-%02d\n", //this is a calender
+            today->tm_year + 1900,
+            today->tm_mon + 1,
+            today->tm_mday);
+
+
+        printf("%02d:%02d:%02d\n", //this is the clock
+            local->tm_hour,
+            local->tm_min,
+            local->tm_sec);
+
+        fflush(stdout); //flush the metaphorical drain
+        gsleep(2);
+    }
+
+    return 0;
+}
