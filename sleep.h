96ea2b4cb506dab2293917c54bd5b235e0370406
VIPAS <voiddevr@protonmail.com>
2026-08-07 21:11:48 -0400

    we must do away with platform-specific sleep

---
+++ sleep.h
@@ -0,0 +1,11 @@
+/*You must include these files in the same directory as the program.
+ *In order to use the program, you must type a header:
+    '#include "sleep.c"::*/
+
+#ifndef GSLEEP_H
+#define GSLEEP_H
+
+//DECLARES THE FUNCTION SO OTEHR C FILES CAN EE IT
+void gsleep(double seconds);
+
+#endif //GSLEEP_H
